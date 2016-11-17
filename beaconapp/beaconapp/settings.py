e21916f137384d745e30d58a68f8c9cce0ee66f8
Costin Bleotu <costi@databus.pro>
2016-11-17 19:01:34 +0200

    sqllite location

--- beaconapp/beaconapp/settings.py
+++ beaconapp/beaconapp/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'beaconapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/var/www/beaconapp','db.sqlite3'),
     }
 }
 
